704d297e2d71b0b8ee081995103edc5a521f6206
Nakao2k <abcdown55@outlook.jp>
2022-04-19 02:34:09 +0900

    test

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/Nakao2k/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('developer@nakao5.site')
         }
